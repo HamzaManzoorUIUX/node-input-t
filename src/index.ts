b97ff64aed57006dc0db75a5c5eaa2dd70d96288
Hamza Manzoor <hamzamanzoor8234@gmail.com>
2024-08-21 23:15:06 +0500

    fin input as class

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-const InputValue = async (message: string): Promise<string | void> => {
-  const response = await rl.question(message, (answer) => {
-    return answer;
-  });
-  return response;
-};
-export { InputValue };
+class Input {
+  public text = async (message: string): Promise<string | void> => {
+    const response = await rl.question(message, (answer) => {
+      return answer;
+    });
+    return response;
+  };
+}
+export default Input;
